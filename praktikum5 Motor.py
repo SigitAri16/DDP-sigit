aa79d1dfea3736fe7d40ab17bc492ea902177f34
Sigit Ariyanto <146793287+SigitAri16@users.noreply.github.com>
2023-10-26 21:18:48 +0700

    Add files via upload

---
+++ praktikum5 Motor.py
@@ -0,0 +1,12 @@
+kendaraan = ["B 6500 ERS","Vario","Honda","160","Hitam"]
+print(kendaraan)
+kendaraan.append("28jt")
+kendaraan.append("jumlah_ban 2")
+print(kendaraan)
+kendaraan.insert (2,"28jt")
+kendaraan.insert(3,"jumlah_ban 2")
+print(kendaraan)
+
+
+
+
